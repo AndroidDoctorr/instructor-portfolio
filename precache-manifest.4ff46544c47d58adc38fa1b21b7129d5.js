48d80c8505c4effa6bb9396a1b6cf13d519deab4
AndroidDoctorr <gbartthenerd@gmail.com>
2020-10-19 00:20:28 -0400

    Updates

---
+++ precache-manifest.4ff46544c47d58adc38fa1b21b7129d5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0384bf7b34c92c24713f8c29f3e8d432",
+    "url": "/instructor-portfolio/index.html"
+  },
+  {
+    "revision": "5a9a36b561b9930f80f0",
+    "url": "/instructor-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d64800d2a83672af2830",
+    "url": "/instructor-portfolio/static/css/main.ba96cd44.chunk.css"
+  },
+  {
+    "revision": "5a9a36b561b9930f80f0",
+    "url": "/instructor-portfolio/static/js/2.82fac969.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/instructor-portfolio/static/js/2.82fac969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64800d2a83672af2830",
+    "url": "/instructor-portfolio/static/js/main.dbc1f3b9.chunk.js"
+  },
+  {
+    "revision": "8123ddff0b2d7ba7133c",
+    "url": "/instructor-portfolio/static/js/runtime-main.dc0aef69.js"
+  },
+  {
+    "revision": "0fe67d4dee76468dfd61fcbeab2c327c",
+    "url": "/instructor-portfolio/static/media/agile-project.0fe67d4d.png"
+  },
+  {
+    "revision": "629d8f882bf90b9f5b82b58c5b1963fe",
+    "url": "/instructor-portfolio/static/media/code-similarity.629d8f88.png"
+  },
+  {
+    "revision": "0fab20094e353050d79fd781bd54aec7",
+    "url": "/instructor-portfolio/static/media/highpoint3.0fab2009.png"
+  },
+  {
+    "revision": "47d182fd853756fb7d0b4f06276df6c9",
+    "url": "/instructor-portfolio/static/media/legoteam.47d182fd.png"
+  },
+  {
+    "revision": "4f45fd10187b84618e963a7943656031",
+    "url": "/instructor-portfolio/static/media/me.4f45fd10.jpg"
+  },
+  {
+    "revision": "3a31ea873f92bcfea585b9c33b5f01c9",
+    "url": "/instructor-portfolio/static/media/rick-and-morty.3a31ea87.png"
+  },
+  {
+    "revision": "fb10c197fe1b937fac80851a76d20cc1",
+    "url": "/instructor-portfolio/static/media/scratch-1.fb10c197.png"
+  },
+  {
+    "revision": "9a832d65306ede258f63eaa5cf40958e",
+    "url": "/instructor-portfolio/static/media/vericite-report.9a832d65.png"
+  }
+]);
